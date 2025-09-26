c2dec94ea5ddb13e8c1a5b5428c3c3f7ae4a3039
benjaminpMugisha <mugishapriib@gmail.com>
2025-09-26 15:51:41 +0200

    Create Insert_Data.sql

---
+++ scripts_sql/Insert_Data.sql
@@ -0,0 +1,27 @@
+
+INSERT INTO customers  VALUES
+(001, 'Kigali Mart', 'Kigali'),
+(002, 'Musanze Hotel', 'Northern'),
+(003, 'URWIBUTSO-Sina Gerard', 'Northern'),
+(004, 'Huye Supermarket', 'Southern'),
+(005, 'Kayonza Distributor', 'Eastern');
+
+INSERT INTO products  VALUES
+(101, 'Akabanga Chili Oil', 'Condiments'),
+(102, 'AKACU Tomato Ketchup', 'Condiments'),
+(103, 'AGASHYA Natural Fruit Juices', 'Beverages'),
+(104, 'Urwibutso Mango Juice', 'Beverages'),
+(105, 'AKANDI Natural Water', 'Beverages'),
+(106, 'AKARUSHO Grape Wine', 'Beverages'),
+(107, 'AKANOZO Flour', 'Bakery');
+
+INSERT INTO sales VALUES
+(201, 001, 101, '2024-03-25', 50, 125000),
+(202, 003,103, '2023-01-20', 30, 90000),
+(203, 002, 102, '2025-02-20', 100, 200000),
+(204, 001, 107, '2021-02-13', 40, 100000),
+(205, 004, 104, '2024-12-15', 60, 150000),
+(206, 005, 106, '2022-03-05', 80, 120000),
+(207, 002, 105, '2023-03-10', 70, 175000),
+(208, 001, 103, '2024-03-15', 45, 135000);
+
